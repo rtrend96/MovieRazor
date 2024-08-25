f203c261c5da1e0f3959eea7fdc62a273771e510
rtrend96 <rtrend96@gmail.com>
2024-08-25 14:36:27 -0500

    Initial commit

---
+++ Data/MovieRazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieRazor.Models;
+
+namespace MovieRazor.Data
+{
+    public class MovieRazorContext : DbContext
+    {
+        public MovieRazorContext (DbContextOptions<MovieRazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieRazor.Models.Movie> Movie { get; set; } = default!;
+    }
+}
